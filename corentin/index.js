f4a1dba0686f29a239803bbd60303132f9385bb7
Tarchaud <101600128+Tarchaud@users.noreply.github.com>
2024-01-09 12:28:22 +0100

    update

--- corentin/index.js
+++ corentin/index.js
@@ -1,5 +1,7 @@
 
 const puppeteer = require('puppeteer');
+const keywords = require('./keywords.json');
+const fs = require('fs');
 
 async function scrapeTripAdvisorReviews(url) {
   const browser = await puppeteer.launch({
@@ -7,7 +9,7 @@ async function scrapeTripAdvisorReviews(url) {
   });
   const page = await browser.newPage();
 
-  await page.goto(url, { waitUntil: 'networkidle0' });
+  await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000  });
 
   //Part PopUp Cookie
   // Attente du sélecteur du bouton "J'accepte" pour les cookies
@@ -16,7 +18,7 @@ async function scrapeTripAdvisorReviews(url) {
   // Clic sur le bouton "J'accepte" pour fermer la fenêtre pop-up des cookies
   await page.click('#onetrust-accept-btn-handler');
 
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(2000);
   // Fin Part PopUp Cookie
 
   let reviews = [];
@@ -31,7 +33,7 @@ async function scrapeTripAdvisorReviews(url) {
         await span.evaluate(span => {
           span.click(); // Clic via JavaScript natif
         }, span);
-        await page.waitForTimeout(1000); // Attendre un court instant pour le chargement
+        await page.waitForTimeout(500); // Attendre un court instant pour le chargement
       }
     }
     //Fin Part pour cliquer sur "Voir plus" pour afficher le commentaire entier
@@ -73,7 +75,7 @@ async function scrapeTripAdvisorReviews(url) {
     ]);
 
     // Attendre un court instant pour la mise à jour des avis
-    await page.waitForTimeout(200); // Vous pouvez ajuster ce délai en fonction de la vitesse de chargement
+    await page.waitForTimeout(2000); // Vous pouvez ajuster ce délai en fonction de la vitesse de chargement
   }
 
   await browser.close();
@@ -81,21 +83,60 @@ async function scrapeTripAdvisorReviews(url) {
   return reviews;
 }
 
-const url = 'https://www.tripadvisor.fr/Restaurant_Review-g187087-d10067617-Reviews-O_Petit_Pau-Pau_Communaute_d_Agglomeration_Pau_Pyrenees_Bearn_Basque_Country_Pyr.html';
-// const url = 'https://www.tripadvisor.fr/Restaurant_Review-g187087-d23285223-Reviews-Yellow_Peak-Pau_Communaute_d_Agglomeration_Pau_Pyrenees_Bearn_Basque_Country_Pyr.html';
 
-scrapeTripAdvisorReviews(url)
-  .then(reviews => {
+async function categorizeReview(reviewText) {
+  for (const category in keywords) {
+      const categoryKeywords = keywords[category];
+      if (categoryKeywords.some(keyword => reviewText.toLowerCase().includes(keyword))) {
+        console.log("Category : ", category);
+        console.log(reviewText);
+        return category;
+      }
+  }
+  return "uncategorized";
+}
+
+
+
+// const url = 'https://www.tripadvisor.com/Restaurant_Review-g187087-d10067617-Reviews-O_Petit_Pau-Pau_Communaute_d_Agglomeration_Pau_Pyrenees_Bearn_Basque_Country_Pyr.html';
+// const url = 'https://www.tripadvisor.com/Restaurant_Review-g60763-d13504265-Reviews-Boucherie_Union_Square-New_York_City_New_York.html';
+const url = 'https://www.tripadvisor.com/Restaurant_Review-g60763-d12425739-Reviews-Piccola_Cucina_Estiatorio-New_York_City_New_York.html';
+
+(async () => {
+  try {
+    const reviews = await scrapeTripAdvisorReviews(url);
     console.log('Total des avis récupérés:', reviews.length);
     
     // Convertir les avis en format JSON
     const reviewsJSON = JSON.stringify(reviews, null, 2);
     
-    // Afficher les avis au format JSON
-    // console.log('Avis au format JSON:', reviewsJSON);
+    // Créer un tableau pour stocker les avis avec leurs catégories
+    const resultat_reviews = [];
 
-    // Retourner les avis au besoin
+    // Fonction asynchrone pour obtenir la catégorie de chaque avis
+    async function processReviews() {
+      for (const review of reviews) {
+        // console.log(review.reviewText);
+        const category = await categorizeReview(review.reviewText);
+        const text = review.reviewText;
+        // console.log(category);
+        resultat_reviews.push({ text, category });
+      }
+    }
+
+    // Appel de la fonction pour traiter les avis avec leurs catégories
+    await processReviews();
+
+    // Affichage des avis au format JSON
+
+    // console.log('Avis au format JSON:', resultat_reviews);
+
+    // Écriture des avis au format JSON dans un fichier
+    fs.writeFileSync('resultat_reviews.json', JSON.stringify(resultat_reviews, null, 2));
+    
+    // Retourner les avis au besoin (non utilisé ici)
     return reviewsJSON;
-  })
-  .catch(error => console.error('Erreur lors de l\'extraction des avis:', error));
-
+  } catch (error) {
+    console.error('Erreur lors de l\'extraction des avis:', error);
+  }
+})();
